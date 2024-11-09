dfc47936116484c5c880488572371b78820150a4
Thomas David <thomasdavid0@gmail.com>
2024-11-09 10:45:05 +0000

    na

--- flightdata/base/__init__.py
+++ flightdata/base/__init__.py
@@ -1,9 +1,12 @@
 from .table import Table, Constructs, SVar
 from .collection import Collection
 from .numpy_encoder import NumpyEncoder
+from numbers import Number
 
 def to_list(obj):
     if hasattr(obj, 'tolist'):
         return obj.tolist()
+    elif obj is None:
+        return None
     else:
         return list(obj)
