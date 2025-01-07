21066a8fccf956fd85f1816a8a8b15f77a42208a
Thomas David <thomasdavid0@gmail.com>
2025-01-07 15:50:07 +0000

    fix flightline script

--- src/flightdata/scripts/flightline.py
+++ src/flightdata/scripts/flightline.py
@@ -1,8 +1,10 @@
+from ast import In
+import sys
 from flightdata import Flight, Origin
 from geometry import GPS
 from pathlib import Path
 import argparse
-
+import re
 
 
 def get_con_groups(log: Flight, channel: int):
@@ -14,18 +16,60 @@ def box_from_log(log: Flight, channel: int):
     grps = get_con_groups(log, channel)
     pilot = grps[0]
     centre = grps[1]
-#    c6on = Flight(log.data.loc[log.data[f'rcin_c{channel}']>=1500])
-#    groups = (c6on.time_flight.diff() > 1).cumsum()
-#    pilot = Flight(c6on.data.loc[groups==0])
-#    centre = Flight(c6on.data.loc[groups==1])
 
     return Origin.from_points("new", GPS(pilot.pos)[-1], GPS(centre.pos)[-1])
 
 def box_from_logs(pilot: Flight, centre: Flight):
     return Origin.from_points("new", GPS(*pilot.pos.iloc[-1]), GPS(*centre.pos.iloc[-1]))
 
+re_logid = re.compile(r'[\d+]$')
+def get_bin_from_number(folder: Path, number: str):
+    try:
+        return list(folder.glob(f"*{number}.BIN"))[0]
+    except IndexError:
+        return list(folder.glob(f"*{number}.bin"))[0]
 
-def main():
+
+
+def create_flightline(args) -> argparse.Namespace:
+    logdir = Path(args.logdir)
+
+    plog = logdir / args.pilot
+    if not plog.exists():
+        if args.pilot is None:
+            plog=sorted(list(logdir.glob("*.BIN")))[0]
+        elif args.pilot.isdigit():
+            plog = get_bin_from_number(logdir, args.pilot)
+        else:
+            raise FileNotFoundError(f'Could not find pilot log file: {args.pilot}')
+    
+    pilot = Flight.from_log(plog)
+
+    print(f'Pilot position log: {plog}')
+
+    if args.centre:
+        clog = logdir / args.centre
+        if not clog.exists():
+            if args.centre is None:
+                clog=None
+            elif args.centre.isdigit():
+                clog = get_bin_from_number(logdir, args.centre)
+        
+        print(f'Centre position log: {clog}' if clog else "No centre position log")
+        centre = Flight.from_log(clog) if clog else None
+        
+        box = Origin.from_points("new", GPS(*pilot.pos.iloc[-1]), GPS(*centre.pos.iloc[-1]))
+    else:
+        if args.direction:
+            box = Origin("new", GPS(plog.pos)[-1], float(args.direction))
+        else:
+            groups = get_con_groups(pilot, args.input)
+            box = Origin.from_points("new", GPS(groups[0].pos)[-1], GPS(groups[1].pos)[-1])
+            
+    return box, plog.stem
+
+
+def parse_args(args):
     parser = argparse.ArgumentParser(description='A tool for creating a flightline .f3a file from bin logs')
 
     parser.add_argument('-l', '--logdir', default='', help='folder to look for logs in')
@@ -34,44 +78,16 @@ def main():
     parser.add_argument('-d', '--direction', default=None, help='heading of the box, if this is specified only pilot will be read')
     parser.add_argument('-i', '--input', default=6, help='channel used to indicate pilot or centre postions (pwm>=1500), None for two files')
 
-    args = parser.parse_args()
+    args = parser.parse_args(args)
 
-    print(args)
+    return args
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+    box, name = create_flightline(args)
+    box.to_f3a_zone(Path(args.logdir) / f'box_{name}.f3a')
     
-    logs = sorted(list(Path(args.logdir).glob("*.BIN")))
-    logids = [int(log.stem) for log in logs]
-
-    if args.pilot in logs:
-        plog = args.pilot
-    elif args.pilot is None:
-        plog=logs[0]
-    elif args.pilot.isdigit():
-        plog = logs[logids.index(int(args.pilot))]
-
-    pilot = Flight.from_log(plog)
-
-    print(f'Pilot position log: {plog}')
-
-    if args.centre in logs:
-        clog = args.centre
-    elif args.centre is None:
-        clog=None
-    elif args.centre.isdigit():
-        clog = logs[logids.index(int(args.centre))]
-    if clog:
-        centre = Flight.from_log(clog)
-    print(f'Centre position log: {clog}')
-
-    if args.centre:
-        box = Origin.from_points("new", GPS(*pilot.pos.iloc[-1]), GPS(*centre.pos.iloc[-1]))
-    else:
-        groups = get_con_groups(pilot, args.input)
-        if args.direction:
-            box = Origin("new", GPS(groups[0].pos)[-1], float(args.direction))
-        else:
-            box = Origin.from_points("new", GPS(groups[0].pos)[-1], GPS(groups[1].pos)[-1])
-            
-    box.to_f3a_zone(Path(args.logdir) / f'box_{plog.stem}.f3a')
 
 if __name__ == '__main__':
     main()
