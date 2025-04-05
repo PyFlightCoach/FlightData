b76418af4f749f9fae25aef6f6c841f84918ad9c
Thomas David <thomasdavid0@gmail.com>
2025-04-05 16:07:04 +0100

    bugfix

--- src/flightdata/state/state.py
+++ src/flightdata/state/state.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Literal, ClassVar, overload
 
-from matplotlib.pyplot import cla
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
